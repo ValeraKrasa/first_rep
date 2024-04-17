2143c8e9924848ef16479d9a54452f9f76d1afba
Valerii <valerakrasa242@gmail.com>
2024-04-17 20:26:38 +0200

    new var1

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ def add_numbers(num1, num2):
 
     num1 = int(input('Enter number 1 - '))
     num2 = int(input('Enter number 1 - '))
-    result = add_numbers(num1, num2)
-    return num1 + num2
+    result = num1 + num2
+    return result
 
 
 
