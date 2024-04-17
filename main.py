44dec47cd8039480d3f6f5e0f655d10ca7a20b0d
Valerii <valerakrasa242@gmail.com>
2024-04-17 20:24:20 +0200

    new variant

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 
 def add_numbers(num1, num2):
+
+    num1 = int(input('Enter number 1 - '))
+    num2 = int(input('Enter number 1 - '))
+    result = add_numbers(num1, num2)
     return num1 + num2
 
-num1 = int(input('Enter number 1 - '))
-num2 = int(input('Enter number 1 - '))
 
-result = add_numbers(num1, num2)
 
-print(f'Result {num1} + {num2} = {result}')
+
+print(f'Result {add_numbers()}')
